f7a9e526458bdf75147c5f6e7f55e4e59f861215
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2020-12-09 23:46:59 +0100

    Add initial files. correct typos

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,11 +1,12 @@
-
 # todo/admin.py
-    
+
 from django.contrib import admin
-from .models import Todo # add this
-    
+from .models import Todo  # add this
+
+
 class TodoAdmin(admin.ModelAdmin):  # add this
-  list_display = ('title', 'description', 'completed') # add this
-        
+    list_display = ('title', 'description', 'completed')  # add this
+
+
 # Register your models here.
-admin.site.register(Todo, TodoAdmin) # add this
+admin.site.register(Todo, TodoAdmin)  # add this
