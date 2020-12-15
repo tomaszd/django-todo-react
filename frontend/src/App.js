002b287ca43a48d883b3a746791d25da6bbc9a5d
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2020-12-15 14:40:10 +0100

    Better truncating of long names . Code formating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
 // frontend/src/App.js
-
 import React, { Component } from "react";
 import Modal from "./components/Modal";
 import axios from "axios";
 
-
-const BACKEND_URL="https://djangodwadwa.herokuapp.com"
-
+const BACKEND_URL = "https://djangodwadwa.herokuapp.com";
 
 class App extends Component {
   constructor(props) {
@@ -16,39 +13,46 @@ class App extends Component {
       activeItem: {
         title: "",
         description: "",
-        completed: false
+        completed: false,
       },
-      todoList: []
+      todoList: [],
     };
   }
   componentDidMount() {
     this.refreshList();
   }
+  truncate = function (str) {
+    let max_length = 40;
+    return str.length > max_length ? str.substring(0, max_length - 3) + "..." : str;
+  };
   refreshList = () => {
     axios
-      .get(BACKEND_URL +"/api/todos/")
-      .then(res => this.setState({ todoList: res.data }))
-      .catch(err => console.log(err));
+      .get(BACKEND_URL + "/api/todos/")
+      .then((res) =>
+        this.setState({
+          todoList: res.data,
+        })
+      )
+      .catch((err) => console.log(err));
   };
-  displayCompleted = status => {
+
+  displayCompleted = (status) => {
     if (status) {
-      return this.setState({ viewCompleted: true });
+      return this.setState({
+        viewCompleted: true,
+      });
     }
-    return this.setState({ viewCompleted: false });
+    return this.setState({
+      viewCompleted: false,
+    });
   };
   renderTabList = () => {
     return (
       <div className="my-5 tab-list ">
-        <span
-          onClick={() => this.displayCompleted(true)}
-          className={this.state.viewCompleted ? "active" : ""}
-        >
-          Show completed
+        <span onClick={() => this.displayCompleted(true)} className={this.state.viewCompleted ? "active" : ""}>
+          Show completed{" "}
         </span>
-        <span
-          onClick={() => this.displayCompleted(false)}
-          className={this.state.viewCompleted ? "" : "active"}
-        >
+        <span onClick={() => this.displayCompleted(false)} className={this.state.viewCompleted ? "" : "active"}>
           Show tasks to do
         </span>
         <button onClick={this.createItem} className="btn btn-primary mr-20">
@@ -59,80 +63,68 @@ class App extends Component {
   };
   renderItems = () => {
     const { viewCompleted } = this.state;
-    const newItems = this.state.todoList.filter(
-      item => item.completed === viewCompleted
-    );
-    return newItems.map(item => (
-      <li
-        key={item.id}
-        className="list-group-item d-flex justify-content-between align-items-center"
-      >
+    const newItems = this.state.todoList.filter((item) => item.completed === viewCompleted);
+    return newItems.map((item) => (
+      <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
         <span
-          className={`todo-title mr-2 ${
-            this.state.viewCompleted ? "completed-todo" : ""
-          }`}
+          className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""}`}
           title={item.description}
         >
-          {item.title}
+          {this.truncate(item.title)}{" "}
         </span>
         <span>
-         <button
-            onClick={() => this.handleToggle(item)}
-            className="btn btn-secondary mr-2"
-          >
-            {item.completed ? 'Not completed': 'Complete Task'}
+          <button onClick={() => this.handleToggle(item)} className="btn btn-secondary mr-2">
+            {item.completed ? "Not completed" : "Complete Task"}
+          </button>{" "}
+          <button onClick={() => this.editItem(item)} className="btn btn-primary mr-2">
+            Edit{" "}
+          </button>{" "}
+          <button onClick={() => this.handleDelete(item)} className="btn btn-danger">
+            Delete{" "}
           </button>
-          <button
-            onClick={() => this.editItem(item)}
-            className="btn btn-primary mr-2"
-          >
-            Edit
-          </button>
-          <button
-            onClick={() => this.handleDelete(item)}
-            className="btn btn-danger"
-          >
-            Delete
-          </button>
-        </span>
+        </span>{" "}
       </li>
     ));
   };
   toggle = () => {
-    this.setState({ modal: !this.state.modal });
+    this.setState({
+      modal: !this.state.modal,
+    });
   };
-  handleSubmit = item => {
+  handleSubmit = (item) => {
     this.toggle();
     if (item.id) {
-      axios
-        .put(BACKEND_URL + `/api/todos/${item.id}/`, item)
-        .then(res => this.refreshList());
+      axios.put(BACKEND_URL + `/api/todos/${item.id}/`, item).then((res) => this.refreshList());
       return;
     }
-    axios
-      .post(BACKEND_URL + "/api/todos/", item)
-      .then(res => this.refreshList());
+    axios.post(BACKEND_URL + "/api/todos/", item).then((res) => this.refreshList());
   };
-  handleDelete = item => {
-    axios
-      .delete(BACKEND_URL +`/api/todos/${item.id}`)
-      .then(res => this.refreshList());
+  handleDelete = (item) => {
+    axios.delete(BACKEND_URL + `/api/todos/${item.id}`).then((res) => this.refreshList());
   };
   createItem = () => {
-    const item = { title: "", description: "", completed: false };
-    this.setState({ activeItem: item, modal: !this.state.modal });
+    const item = {
+      title: "",
+      description: "",
+      completed: false,
+    };
+    this.setState({
+      activeItem: item,
+      modal: !this.state.modal,
+    });
   };
-  editItem = item => {
-    this.setState({ activeItem: item, modal: !this.state.modal });
+  editItem = (item) => {
+    this.setState({
+      activeItem: item,
+      modal: !this.state.modal,
+    });
   };
-  handleToggle = item => {
+  handleToggle = (item) => {
     console.log(item);
-    item.completed=!item.completed;
+    item.completed = !item.completed;
     console.log(item.completed);
     if (item.id) {
-      axios
-        .put(BACKEND_URL + `/api/todos/${item.id}/`, item)
-        .then(res => this.refreshList());
+      axios.put(BACKEND_URL + `/api/todos/${item.id}/`, item).then((res) => this.refreshList());
       return;
     }
   };
@@ -140,25 +132,17 @@ class App extends Component {
   render() {
     return (
       <main className="content">
-        <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
+        <h1 className="text-white text-uppercase text-center my-4"> Todo app </h1>{" "}
         <div className="row ">
           <div className="col-md-6 col-sm-10 mx-auto p-0">
             <div className="card p-3">
-
-              {this.renderTabList()}
-              <ul className="list-group list-group-flush">
-                {this.renderItems()}
-              </ul>
-            </div>
-          </div>
-        </div>
+              {this.renderTabList()} <ul className="list-group list-group-flush"> {this.renderItems()} </ul>{" "}
+            </div>{" "}
+          </div>{" "}
+        </div>{" "}
         {this.state.modal ? (
-          <Modal
-            activeItem={this.state.activeItem}
-            toggle={this.toggle}
-            onSave={this.handleSubmit}
-          />
-        ) : null}
+          <Modal activeItem={this.state.activeItem} toggle={this.toggle} onSave={this.handleSubmit} />
+        ) : null}{" "}
       </main>
     );
   }
