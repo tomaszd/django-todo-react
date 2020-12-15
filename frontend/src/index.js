6f95f74e6b60d37d289899b03c8caa5426572a7b
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2020-12-15 01:18:03 +0100

    new site title

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,9 @@
   import './index.css';
   import App from './App';
   import * as serviceWorker from './serviceWorker';
-  
+
+  document. title = 'Task Manager';
+
   ReactDOM.render(<App />, document.getElementById('root'));
   // If you want your app to work offline and load faster, you can change
   // unregister() to register() below. Note this comes with some pitfalls.
